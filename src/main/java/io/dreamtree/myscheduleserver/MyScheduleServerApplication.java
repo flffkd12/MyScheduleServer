3d1b46e0ec027a68afe148d2358c7ef887abf53a
flffkd12 <flffkd12@gmail.com>
2024-08-16 00:05:02 +0900

    Initial commit

---
+++ src/main/java/io/dreamtree/myscheduleserver/MyScheduleServerApplication.java
@@ -0,0 +1,13 @@
+package io.dreamtree.myscheduleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScheduleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScheduleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dreamtree/myscheduleserver/MyScheduleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dreamtree.myscheduleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScheduleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
