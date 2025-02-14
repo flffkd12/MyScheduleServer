e81cb476085fa78154585afdf346cae9c5ad6778
flffkd12 <flffkd12@gmail.com>
2025-02-14 21:04:13 +0900

    Initial commit

---
+++ src/main/java/com/example/MyScheduleServer/MyScheduleServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyScheduleServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyScheduleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyScheduleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MyScheduleServer/MyScheduleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyScheduleServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyScheduleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
