ccf0852dabe5803fd37c392626c458fffeb5a68b
flffkd12 <flffkd12@gmail.com>
2025-04-02 23:38:06 +0900

    feat: Contoller에서 검사할 항목 추가
    * 유효한 시간에 대해 날씨 자료 요청했는지 검사
    * 데이터가 만들어지거나 실패하는 경우 세분화

--- src/main/java/com/example/MyScheduleServer/controller/WeatherController.java
+++ src/main/java/com/example/MyScheduleServer/controller/WeatherController.java
@@ -2,8 +2,16 @@ package com.example.MyScheduleServer.controller;
 
 import com.example.MyScheduleServer.dto.WeatherItem;
 import com.example.MyScheduleServer.service.WeatherService;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,20 +28,61 @@ public class WeatherController {
   private final String SERVICE_KEY = "%2B4Prahol80blyOZ%2F4erVmmDgGmDb2KbjalyKgd9cRGOE5HvVkBLRetPwt93SXayZiPzA4Huut%2FWmCIwyfJ1mOg%3D%3D";
 
   @GetMapping("/weather")
-  public ResponseEntity<List<WeatherItem>> getWeatherInfo(
+  public ResponseEntity<Map<String, Object>> getWeatherInfo(
       @RequestParam("baseDate") String baseDate,
       @RequestParam("baseTime") String baseTime,
       @RequestParam("nx") String nx,
       @RequestParam("ny") String ny
   ) {
+    List<String> validBaseTime = List.of("0200", "0500", "0800", "1100", "1400", "1700", "2000",
+        "2300");
+    if (!validBaseTime.contains(baseTime)) {
+      return ResponseEntity.badRequest()
+          .body(Map.of("message",
+              "Invalid baseTime query parameter. baseTime must be one of: " + Arrays.toString(
+                  validBaseTime.toArray())));
+    }
+
+    try {
+      LocalDateTime requestedDateTime = LocalDateTime.parse(baseDate + baseTime,
+          DateTimeFormatter.ofPattern("yyyyMMddHHmm"));
+      LocalDateTime now = LocalDateTime.now();
+      if (requestedDateTime.isAfter(now.minusMinutes(10)) || requestedDateTime.isBefore(
+          now.minusDays(1))) {
+        return ResponseEntity.badRequest().body(Map.of("message", "Invalid baseDate or baseTime. "
+            + "Weather information is available 10 minutes after the requested time, which must be within 24 hours past."));
+      }
+    } catch (DateTimeParseException e) {
+      // parse
+    } catch (IllegalArgumentException e) {
+      // ofPattern
+    } catch (DateTimeException e) {
+      //10분 빼는 검사  /// 주요 excetion에 대해서 최대 2,3개 쓰고 나머지는 포괄적인 Exception으로 잡자 for 가동성
+    }
+
     String url = API_URL + "?serviceKey=" + SERVICE_KEY + "&numOfRows=900&pageNo=1&dataType=JSON"
         + "&base_date=" + baseDate + "&base_time=" + baseTime + "&nx=" + nx + "&ny=" + ny;
 
     try {
       List<WeatherItem> response = weatherService.processWeatherData(url);
-      return ResponseEntity.ok(response);
+      if (response.isEmpty()) {
+        return ResponseEntity.badRequest().body(Map.of("message",
+            "Can't load weather data for this location. Check nx, ny query parameter for valid location."));
+      } else {
+        // 데이터가 존재하니 데이터에 대한 가공
+        return ResponseEntity.ok(Map.of("data", response));
+      }
     } catch (MalformedURLException e) {
-      return ResponseEntity.badRequest().body(null); // 적합하지 않은 쿼리 파라미터
+      e.printStackTrace();
+      return ResponseEntity.badRequest().body(
+          Map.of("message", "Invalid parameter. Check the query parameter format."));
+    } catch (ProtocolException e) {
+      e.printStackTrace();
+      // 클라이언트와 서버간 프로토콜 버전 일치 확인
+      // 공공데이터 포탈은 GET만 수신하는데 다른거 썼는지 확인
+    } catch (IOException e) {
+      e.printStackTrace();
+      // 네트워크 오류, 시간 초과, 서비스 키 만료, API URL변경
     }
   }
 }
--- src/main/java/com/example/MyScheduleServer/service/WeatherService.java
+++ src/main/java/com/example/MyScheduleServer/service/WeatherService.java
@@ -17,49 +17,40 @@ import org.springframework.stereotype.Service;
 @Service
 public class WeatherService {
 
-  public List<WeatherItem> processWeatherData(String url) throws MalformedURLException {
+  public List<WeatherItem> processWeatherData(String url)
+      throws MalformedURLException, ProtocolException, IOException {
+
+    URL dataUrl = new URL(url);
+    HttpURLConnection conn = (HttpURLConnection) dataUrl.openConnection();
+    conn.setRequestMethod("GET");
+    conn.setRequestProperty("Content-type", "application/json");
+
+    BufferedReader rd;
+    if (conn.getResponseCode() >= 200 && conn.getResponseCode() < 300) {
+      rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+    } else {
+      rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+    }
+
+    StringBuilder sb = new StringBuilder();
+    String line;
+    while ((line = rd.readLine()) != null) {
+      sb.append(line);
+    }
+
+    rd.close();
+    conn.disconnect();
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    JsonNode jsonNode = objectMapper.readTree(sb.toString());
+    JsonNode items = jsonNode.path("response").path("body").path("items").path("item");
+
     List<WeatherItem> weatherItemList = new ArrayList<>();
-
-    try {
-      URL dataUrl = new URL(url);
-      HttpURLConnection conn = (HttpURLConnection) dataUrl.openConnection();
-      conn.setRequestMethod("GET");
-      conn.setRequestProperty("Content-type", "application/json");
-
-      BufferedReader rd;
-      if (conn.getResponseCode() >= 200 && conn.getResponseCode() < 300) {
-        rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-      } else {
-        rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+    if (items.isArray()) {
+      for (JsonNode item : items) {
+        WeatherItem weatherItem = objectMapper.treeToValue(item, WeatherItem.class);
+        weatherItemList.add(weatherItem);
       }
-
-      StringBuilder sb = new StringBuilder();
-      String line;
-      while ((line = rd.readLine()) != null) {
-        sb.append(line);
-      }
-      rd.close();
-      conn.disconnect();
-
-      ObjectMapper objectMapper = new ObjectMapper();
-      JsonNode jsonNode = objectMapper.readTree(sb.toString());
-      JsonNode items = jsonNode.path("response").path("body").path("items").path("item");
-
-      if (items.isArray()) {
-        for (JsonNode item : items) {
-          WeatherItem weatherItem = objectMapper.treeToValue(item, WeatherItem.class);
-          weatherItemList.add(weatherItem);
-        }
-      }
-    } catch (MalformedURLException e) {
-      throw e;
-    } catch (ProtocolException e) {
-      e.printStackTrace();
-      // 클라이언트와 서버간 프로토콜 버전 일치 확인
-      // 공공데이터 포탈은 GET만 수신하는데 다른거 썼는지 확인
-    } catch (IOException e) {
-      e.printStackTrace();
-      // 네트워크 오류, 시간 초과
     }
 
     return weatherItemList;
